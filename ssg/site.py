2fe8ccf92ff48a5720d6527bdfa726b2e329a6e2
feliksk <feliksk@gmail.com>
2021-05-02 20:14:29 -0700

    test

--- ssg/site.py
+++ ssg/site.py
@@ -13,6 +13,6 @@ class Site:
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
         
-        for path in self.source.rglob('*'):
-            if path.is_dir == True:
+        for path in self.source.rglob("*"):
+            if path.is_dir():
                 create_dir(self, path)
