0b407cd462439430d9a8b5d9f6ded6d244c3d21e
feliksk <feliksk@gmail.com>
2021-05-04 20:16:33 -0700

    module 3

---
+++ ssg/content.py
@@ -0,0 +1,44 @@
+import re
+from yaml import load, FullLoader
+from collections.abc import Mapping
+
+class Content(Mapping):
+    __delimiter = r"^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
+    
+    @classmethod
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
+        load(fm, Loader=FullLoader)
+        cls(metadata, content)
+        
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data.content = content
+        
+    @property
+    def body(self):
+        return self.data["content"]
+    
+    @property
+    def type(self):
+        return self.data["type"] if self.data["type"] else None
+    
+    @type.setter
+    def type(self, type):
+        self.data["type"] = type
+    
+    def __getitem__(self, key):
+        return self.data[key]
+    
+    def __iter__(self):
+        return iter(self.data)
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __repr__(self):
+        data = dict()
+        return str(data)
+    
+    
